00cad48ae4ebdfc3eb83b82abde74df7cfeb645a
Mitra Nami <a.mxnami@gmail.com>
2021-02-02 18:14:46 -0500

    Get location from localstorage

--- storage.js
+++ storage.js
@@ -4,3 +4,16 @@ function saveLocation(city, country) {
   localStorage.setItem('location', JSON.stringify(location));
 }
 
+
+//Get location from local storage
+// returns {city, country} obj
+function getLocation() {
+  const location = localStorage.getItem('location');
+  if (location) {
+    //if the location key exists in local storage
+    return JSON.parse(location);
+  }
+  //location key doesn't exist in local storage(null)
+  //set default to kingston, ca
+  return {city: 'Kingston', country: 'ca'};
+}
